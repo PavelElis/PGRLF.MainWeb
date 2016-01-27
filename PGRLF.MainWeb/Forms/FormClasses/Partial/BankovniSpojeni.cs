813f665df7036a8c3446172df1b455736787c919
Tomáš Mrázek <tomastokamrazek@gmail.com>
2016-01-27 14:58:13 +0100

    update 2

---
+++ PGRLF.MainWeb/Forms/FormClasses/Partial/BankovniSpojeni.cs
@@ -0,0 +1,26 @@
+﻿using Foolproof;
+using PGRLF.MainWeb.Forms.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PGRLF.MainWeb.Forms.FormClasses.Partial
+{
+    public class BankovniSpojeni
+    {
+
+        [Display(GroupName = "spolecnaCast", ResourceType = typeof(FormResources), 
+            Name = "PodporaZemedelec_CisloUctu")]
+        [Required(ErrorMessageResourceType = typeof(FormResources),
+            ErrorMessageResourceName = "Nevyplneno_CisloUctu")]
+        [RegularExpression(Helpers.AccountNumberFormat, ErrorMessageResourceType = typeof(FormResources),
+            ErrorMessageResourceName = "Chyba_CisloUctu")]
+        public string CisloUctu { get; set; }
+
+        [Display(GroupName = "spolecnaCast", ResourceType = typeof(FormResources),
+            Name = "PodporaZemedelec_KodBankyVyberteBanku")]
+        [DataType("Banka")]
+        [Required(ErrorMessageResourceType = typeof(FormResources),
+            ErrorMessageResourceName = "Nevyplneno_KodBanky")]
+        public string KodBanky { get; set; }
+    }
+}
---
+++ PGRLF.MainWeb/Forms/FormClasses/PodnikatelskyZamer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PGRLF.MainWeb.Forms.FormClasses
+{
+    public class PodnikatelskyZamer
+    {
+    }
+}
